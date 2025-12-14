35aec47508251931bf104fc78cad11653314be1e
Kubii <samochodykuby07@gmail.com>
2025-12-14 10:41:20 +0100

    Enhance runner.py with typeof function and logic
    Added 'typeof' built-in function and improved variable initialization logic.

--- runner.py
+++ runner.py
@@ -1,68 +1,103 @@
 import re
 
+from nltk.inference.mace import arguments
+
 lines = []
 keywords = ["var", "const", "func", "return"]
-build_in = ["print", "input", "len"]
+build_in = ["print", "input", "len", "typeof"]
 operators = ["+", "-", "*", "=", "%"]
 punctuators = ['"', '(', ')']
 variables = {}
+
 
 class initVariables:
     def __init__(self, line):
         lineIndex = lines.index(line) + 1
         if 'var' in line:
-            line = line.replace('var', '').strip()
-            equal_index = line.find('=')
-            name = line[:equal_index].strip()
-            value = line[equal_index + 1:].strip()
-            if not variables.get(str(name)):
-                variables.setdefault(str(name), []).append(value) #appending value
-                variables.setdefault(str(name), []).append(self.get_type(value)) #appending type
-                variables.setdefault(str(name), []).append(len(value)) #appending length
+            if any(t for t in build_in):
+                print("func")
             else:
-                print(f"Error in line {lineIndex}: Variable '{name}' is already defined.")
+                line = line.replace('var', '').strip()
+                equal_index = line.find('=')
+                name = line[:equal_index].strip()
+                value = line[equal_index + 1:].strip()
+                if not variables.get(str(name)):
+                    variables.setdefault(str(name), []).append(value)  # appending value
+                    variables.setdefault(str(name), []).append(self.get_type(value))  # appending type
+                    variables.setdefault(str(name), []).append(len(value))  # appending length
+                else:
+                    print(f"Error in line {lineIndex}: Variable '{name}' is already defined.")
         else:
             pass
 
-    def get_type(self, value):
+    @staticmethod
+    def get_type(value):
         if '"' in value or "'" in value:
             return "string"
         elif re.match(r'^\d+(\.\d+)?$', value):
             return "number"
         elif value == "True" or value == "False":
             return "boolean"
-            
+
+    @staticmethod
+    def init_with_function_value():
+        pass
+
 class BuildInFunctions:
     def __init__(self, line, type_of_function):
         self.line = line
         self.type_of_function = type_of_function
-        #get arguments
-        first_bracket = line.find('(')
-        last_bracket = line.rfind(')')
-        self.arguments = line[first_bracket + 1:last_bracket].split(',')
+        # get arguments
+        self.first_bracket = line.find('(')
+        self.last_bracket = line.rfind(')')
+        self.arguments = line[self.first_bracket + 1:self.last_bracket].split(',')
         if self.type_of_function == "print":
-            self.print_func(self.arguments)
+            self.print()
         elif self.type_of_function == "input":
-            self.input_func()
+            self.input()
         elif self.type_of_function == "len":
-            self.len_func()
+            self.len()
+        elif self.type_of_function == "typeof":
+            self.typeof()
 
-    def print_func(self, arguments):
+    def print(self):
+        out = ""
         for i in self.arguments:
-            if '"' in i or "'" in i: #strings
-                print(i.strip().replace('"', '').replace("'", ''))
-            elif re.match(r'^\d+(\.\d+)?$', i.strip()): #numbers
-                print(i)
-            else: #variables
-                var_name = i.strip()
-                if var_name in variables:
-                    print(variables[var_name][0]) #print value
+            if '"' in i or "'" in i:  # strings
+                out += (i.strip().replace('"', '').replace("'", ''))
+            elif re.match(r'^\d+(\.\d+)?$', i.strip()):  # numbers
+                out += i
+            else:  # object
+                if '(' in i: #fuction
+                    self.line = self.line.replace('print', '')
+                    self.line = self.line[1:len(self.line)-1]
+                    id_bracket = self.line.find('(')
+                    func_name = self.line[:id_bracket].strip()
+                    arguments = self.line[id_bracket + 1: len(self.line) - 1].split(',')
+                    if func_name in build_in:
+                        if func_name == 'typeof':
+                            out += getattr(self, "typeof")(arguments[0])
                 else:
-                    print(f"Error: Variable '{var_name}' is not defined.")
-    def input_func(self):
+                    var_name = i.strip()
+                    if var_name in variables:
+                        out += variables[var_name][0]  # print value
+                    else:
+                        print(f"Error: Variable '{var_name}' is not defined.")
+        print(out)
+    def typeof(self, argument):
+        if argument in variables:
+            return variables[argument][1]
+        elif argument.isdigit():
+            return "number"
+        elif '"' in argument:
+            return "string"
+    def input(self, argument):
+        print(f"Succesfully called input function {argument}")
+        if 'var' in self.line:
+            self.line = self.line.replace('var', '').strip()
+    def len(self, argument):
         print("Succesfully called input function")
-    def len_func(self):
-        print("Succesfully called input function")
+
 
 with open("plik.txt", "r", encoding="utf-8") as f:
     for linia in f:
@@ -83,4 +118,4 @@ for line in lines:
         function = BuildInFunctions(line, found_func[0])
         ltype = f"Built-in function call: {', '.join(found_func)}"
 
-    #print(f"Line: {line}\n keywords: {found_keywords} operators: {found_operators} punctuators: {found_punctuators}\n {ltype}\n\n")
+    # print(f"Line: {line}\n keywords: {found_keywords} operators: {found_operators} punctuators: {found_punctuators}\n {ltype}\n\n")
